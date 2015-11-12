c68efd92bc5d04a2584cd94609ab2e064394fbe9
Stefan Hartmann <stefan@wemove.com>
2015-11-12 01:44:46 +0100

    bugfix, prefix new sku

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -3,7 +3,7 @@ Spree::Product.class_eval do
   def try_variant(option_values)
     return master if options.blank?
     new_variant = Spree::Variant.new(product: self,
-                                     sku: "-#{(Time.now.to_f * 1000.0).to_i}",
+                                     sku: "#{self.sku}-#{(Time.now.to_f * 1000.0).to_i}",
                                      track_inventory: false)
     new_variant.add_options_and_calc_price option_values
     return get_or_save_variant new_variant
