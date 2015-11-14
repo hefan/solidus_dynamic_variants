49ded59bc1ae012b64e3a79b7e91dc666daba1fa
Stefan Hartmann <stefan@wemove.com>
2015-11-14 02:39:31 +0100

    used concerns instead of class eval

---
+++ app/models/concerns/dynamic_variants/product_concerns.rb
@@ -0,0 +1,27 @@
+module DynamicVariants::ProductConcerns
+  extend ActiveSupport::Concern
+
+  def try_variant(option_values)
+    return master if options.blank?
+    new_variant = Spree::Variant.new(product: self,
+                                     sku: "#{self.sku}-#{(Time.now.to_f * 1000.0).to_i}",
+                                     track_inventory: false)
+    new_variant.add_options_and_calc_price option_values
+    return get_or_save_variant new_variant
+  end
+
+
+  private
+
+  def get_or_save_variant(new_variant)
+    aspirants = Spree::Variant.includes(:option_values).where(is_master: false,
+                                                              product: self,
+                                                              deleted_at: nil)
+    similar = aspirants.detect { |av| new_variant.similar? av }
+    return similar if similar.present?
+
+    new_variant.save!
+    return new_variant
+  end
+
+end
