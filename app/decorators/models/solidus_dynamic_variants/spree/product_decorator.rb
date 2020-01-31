69c99559b9dc6576a2c1fbb15d8d2ec0c21d10b0
stefan <sh@wemove.com>
2020-01-31 20:24:11 +0100

    new zeitwerk compatible way to name and nest decorators

---
+++ app/decorators/models/solidus_dynamic_variants/spree/product_decorator.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module SolidusDynamicVariants
+  module Spree
+    module ProductDecorator
+      def try_variant(option_values)
+        return master if option_values.blank?
+
+        new_variant = ::Spree::Variant.new(product: self,
+                                         sku: "#{sku}-#{(Time.now.to_f * 1000.0).to_i}",
+                                         track_inventory: false)
+        new_variant.add_options_and_calc_price option_values
+        get_or_save_variant new_variant
+      end
+
+      private
+
+      def get_or_save_variant(new_variant)
+        aspirants = ::Spree::Variant.includes(:option_values).where(is_master: false,
+                                                                  product: self,
+                                                                  deleted_at: nil)
+        similar = aspirants.detect { |av| new_variant.similar? av }
+        return similar if similar.present?
+
+        new_variant.save!
+        new_variant
+      end
+
+      ::Spree::Product.prepend self
+    end
+  end
+end
