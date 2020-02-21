95d058f9ad371269eceff9b4813f0364b2eaf6a0
stefan <sh@wemove.com>
2020-02-21 16:39:17 +0100

    added deface as a requirement

--- lib/solidus_dynamic_variants.rb
+++ lib/solidus_dynamic_variants.rb
@@ -2,6 +2,9 @@
 
 require 'solidus_core'
 require 'solidus_support'
+require 'solidus_frontend'
+require 'solidus_backend'
+require 'deface'
 
 require 'solidus_dynamic_variants/version'
 require 'solidus_dynamic_variants/engine'
