dc6bc704e3a188d8413c8d8353e749caf8395439
Stefan <>
2022-11-01 02:48:20 +0100

    solidus_starter frontend compatibility: move cart to own controller, form view example and docs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-Spree::Core::Engine.routes.draw do
-  resources :orders, except: [:index, :new, :create, :destroy] do
-    post :variant_populate, on: :collection
-  end
+Rails.application.routes.draw do
+  resources :dynamic_cart_line_items, only: :create
 end
--- spec/controllers/dynamic_cart_line_items_controller_spec.rb
+++ spec/controllers/dynamic_cart_line_items_controller_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-describe Spree::OrdersController, type: :controller do
+describe DynamicCartLineItemsController, type: :controller do
   let!(:user) { create(:user) }
   let!(:store) { create(:store) }
   let!(:order) { create(:order) }
@@ -12,9 +12,9 @@ describe Spree::OrdersController, type: :controller do
     allow(controller).to receive_messages(try_spree_current_user: user)
   end
 
-  describe '#variant_populate' do
+  describe '#create' do
     it 'returns 200' do
-      post :variant_populate, params: { product_id: product.id, quantity: 5 }
+      post :create, params: { product_id: product.id, quantity: 5 }
       expect(response.response_code).to eq 302
     end
   end
